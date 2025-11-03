fec05e0cc0b1eba21e5e4fbca83b9b1c62c4fe3d
lavanyash65-lang <lavanyash65@gmail.com>
2025-11-03 14:23:26 +0530

    save

---
+++ simple/simp.py
@@ -0,0 +1,8 @@
+principal = 10000 
+rate = 5            
+time = 2            
+simple_interest = (principal * rate * time) / 100
+print("Principal Amount:", principal)
+print("Rate of Interest:", rate)
+print("Time in years:", time)
+print("Simple Interest:", simple_interest)
