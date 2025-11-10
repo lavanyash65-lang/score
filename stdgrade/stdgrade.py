c6eecc91bfed107974e875ad86e31aeb7147d3a8
lavanyash65-lang <lavanyash65@gmail.com>
2025-11-10 14:46:24 +0530

    save

---
+++ stdgrade/stdgrade.py
@@ -0,0 +1,20 @@
+import sys
+
+def calculate_average(m1, m2, m3, m4, m5):
+    total = m1 + m2 + m3 + m4 + m5
+    average = total / 5
+    print(f"The average marks are: {average:.2f}")
+
+
+if len(sys.argv) != 6:
+    print("Usage: python program.py <mark1> <mark2> <mark3> <mark4> <mark5>")
+    sys.exit(1)
+
+
+m1 = float(sys.argv[1])
+m2 = float(sys.argv[2])
+m3 = float(sys.argv[3])
+m4 = float(sys.argv[4])
+m5 = float(sys.argv[5])
+
+calculate_average(m1, m2, m3, m4, m5)
