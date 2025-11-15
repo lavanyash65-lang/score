522045c3bead6840fb76e768bd2a24c3883f4bab
lavanyash65-lang <lavanyash65@gmail.com>
2025-11-15 20:38:24 +0530

    save

--- jrnl7/score.py
+++ jrnl7/score.py
@@ -5,12 +5,17 @@ if len(sys.argv) > 1:
     print("User provided scores:")
 else:
     print("No input given - using default scores:")
-    scores = ["80", "90", "75"]
+    scores = ["80", "90", "75", "88"]
 
 scores = [eval(x) for x in scores]
+
 total = sum(scores)
 average = total / len(scores)
+maximum = max(scores)
+minimum = min(scores)
 
 print("Scores:", scores)
 print("Sum =", total)
 print("Average =", average)
+print("Maximum =", maximum)
+print("Minimum =", minimum)
